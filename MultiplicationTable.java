5d92d7eb75fe94bb9fa3275137874bf909733c75
Sireesha <bsireesha2903@gmail.com>
2025-06-11 19:07:10 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,16 @@
+package FirstPackage;
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=7;
+		int result;
+		for(int i=1;i<=10;i++) {
+			result=n*i;
+		System.out.println(String.format(n +" x " +i +" = "+result));
+		}
+		
+	}
+
+}
