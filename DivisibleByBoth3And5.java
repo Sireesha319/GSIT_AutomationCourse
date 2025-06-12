5c01224127bf05cb697035cb4c1f322f055fee11
Sireesha <bsireesha2903@gmail.com>
2025-06-12 14:16:38 +0530

    Add files via upload

---
+++ DivisibleByBoth3And5.java
@@ -0,0 +1,15 @@
+package FirstPackage;
+
+public class DivisibleByBoth3And5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//sireesha_Boddu
+		for(int i=1;i<=100;i++) {
+			if(i%3==0 && i%5==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
