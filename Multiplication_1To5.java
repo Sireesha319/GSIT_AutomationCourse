f1fe40d841edec2d44da1e3346cee6f0c2afec3f
Sireesha <bsireesha2903@gmail.com>
2025-06-12 14:03:46 +0530

    Add files via upload

---
+++ Multiplication_1To5.java
@@ -0,0 +1,17 @@
+package FirstPackage;
+
+public class Multiplication_1To5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Sireesha_Boddu
+		int i,j;
+		for(i=1;i<=5;i++) {
+			for (j=1;j<=10;j++) {
+				int result=i*j;
+				System.out.println(i + " x " + j + " = " + result);
+			}
+			System.out.println();
+		}
+	}
+}
